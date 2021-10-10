714f183eb02ce4918ac8bd453d836f9db68ea398
surajkarki66 <suraj.karki500@protonmail.com>
2021-10-10 10:00:08 +0545

    Created mine_block endpoint

--- simple-blockchain/blockchain.py
+++ simple-blockchain/blockchain.py
@@ -70,3 +70,28 @@ class Blockchain:
 
 
 # 2) Mining a blockchain
+
+# Creating a web app
+app = Flask(__name__)
+
+# Creating a Blockchain
+blockchain = Blockchain()
+
+# Mining a new block
+
+
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {
+        'message': 'Congratulations!, you just mined a block!',
+        'index': block['index'],
+        'timestamp': block['timestamp'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash']
+    }
+    return jsonify(response), 200
